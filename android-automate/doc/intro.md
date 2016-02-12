39ea5c85642dbd0212675943e3e338b09def2348
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-02-12 07:19:10 +0100

    Init.

---
+++ android-automate/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to android-automate
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ android-automate/test/android_automate/core_test.clj
@@ -0,0 +1,7 @@
+(ns android-automate.core-test
+  (:require [clojure.test :refer :all]
+            [android-automate.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
